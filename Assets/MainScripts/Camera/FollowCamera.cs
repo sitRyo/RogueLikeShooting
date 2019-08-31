9b01a2d9389792e85666ac2381f1c275fd221619
sitRyo <al15030@shibaura-it.ac.jp>
2019-08-31 16:13:50 +0900

    球面線形補間を使った戦車の移動処理を追加

---
+++ Assets/MainScripts/Camera/FollowCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+
+    private void LateUpdate() {
+        float cx = player.transform.position.x;
+        float cz = player.transform.position.z;
+        transform.position = new Vector3(cx, transform.position.y, cz);
+    }
+}
--- Assets/MainScripts/Tank/TankMove.cs
+++ Assets/MainScripts/Tank/TankMove.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class TankMove : MonoBehaviour {
     
     [SerializeField] private float moveSpeed = 5.0f;
-    [SerializeField] private float rotationSpeed = 5.0f;
+    [SerializeField] private float rotationSpeed = 0.2f;
     [SerializeField] private Vector3 velocity;
     
     
     private void Update() {
-        //float localAngleY = transform.localEulerAngles.y;
-
         velocity = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) 
             velocity.z += 1;
@@ -24,16 +22,13 @@ public class TankMove : MonoBehaviour {
         
         Vector3 normV = velocity.normalized;
 
-        float forwardY = Mathf.Atan2(transform.forward.x, transform.forward.z);
-        float angleY = Mathf.Atan2(normV.x, normV.z);
-
         velocity = velocity.normalized * moveSpeed * Time.deltaTime;
-        transform.position += velocity;
 
-        float rot = angleY - forwardY;
-    
-        // Debug.Log(Mathf.Atan2(transform.forward.x, transform.forward.z));
-        // Debug.Log(Mathf.Atan2(normV.x, normV.z));
-        // Debug.Log(normV);
+        if (velocity.magnitude > 0) {
+            transform.position += velocity;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(velocity), rotationSpeed);
+        }
+
+
     }
 }
