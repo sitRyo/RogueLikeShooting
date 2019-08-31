2a24208da7a322fcc1f145cba3386fd350eabaef
sitRyo <al15030@shibaura-it.ac.jp>
2019-08-31 18:18:13 +0900

    砲弾を生成するスクリプトを追加

--- Assets/MainScripts/Tank/TankMove.cs
+++ Assets/MainScripts/Tank/TankMove.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TankMove : MonoBehaviour {
-    
+
+    class Shell {
+        public GameObject shell;
+        public float elapsed;
+        public bool isLive;
+        private float deadline;
+        
+
+        public Shell (GameObject sh) {
+            shell = sh;
+            elapsed = 0.0f;
+            deadline = 2.0f;
+            isLive = true;
+        }
+
+        public bool check() {
+            if (elapsed > deadline) {
+                isLive = false;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private float rotationSpeed = 0.2f;
     [SerializeField] private Vector3 velocity;
-    
-    
+    [SerializeField] private GameObject m_Shell;
+    [SerializeField] private float ShellSpeed = 15.0f;
+
+    private Vector3 m_ShellPosition;
+    private List<Shell> Shells = new List<Shell>();
+
     private void Update() {
         velocity = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) 
@@ -32,6 +60,26 @@ public class TankMove : MonoBehaviour {
                         rotationSpeed);
         }
 
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            m_ShellPosition = transform.position + transform.forward * 5.0f;
+            GameObject m_InstantiateShell = Instantiate(m_Shell, m_ShellPosition, transform.rotation);
+            Shell sh = new Shell(m_InstantiateShell);
+            Shells.Add(sh);
+        }
 
+        foreach (var sh in Shells) {
+            if (ShellUpdate(sh)) {
+                Destroy(sh.shell);
+            }
+        }
+
+        Shells.RemoveAll(sh => !sh.isLive);
+    }
+
+    private bool ShellUpdate(Shell shell) {
+        shell.elapsed += Time.deltaTime;
+        shell.shell.gameObject.transform.position
+            += shell.shell.gameObject.transform.forward * Time.deltaTime * ShellSpeed;
+        return shell.check();
     }
 }
