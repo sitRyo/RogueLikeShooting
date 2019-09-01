a3a7939b572e1d4e06c016b8a5c897de847174fd
sitRyo <al15030@shibaura-it.ac.jp>
2019-09-01 11:32:56 +0900

    ダンジョン生成スクリプトを初期化, ダンジョンの状態を決めるパラメータ定数を定義

--- Assets/MainScripts/Doungeon/DungeonGenerate.cs
+++ Assets/MainScripts/Doungeon/DungeonGenerate.cs
@@ -1,37 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DungeonConst;
 
-public class DungeonGenerate : MonoBehaviour {
-  
-  public int[ , ] dungeon;
-  public int m_DungeonX { get; private set; }
-  public int m_DungeonY { get; private set; }
+public class DungeonGenerate : MonoBehaviour { 
+    [SerializeField] private int m_MapWidth = 50;
+    [SerializeField] private int m_MapHeight = 50;
 
-  bool flag = true;
-
-  void Start() {
-    int x = Random.Range(30, 40);
-    int y = Random.Range(30, 40);
-
-    m_DungeonX = x;
-    m_DungeonY = y;
-
-    dungeon = new int[y, x];
-  }
-
-  void Update() {
-    if (flag) {
-      for (int i = 0; i < m_DungeonY; ++i) {
-        string log = "";
-        for (int j = 0; j < m_DungeonX; ++j) {
-          int tmp = Random.Range(0, 3);
-          dungeon[i, j] = tmp;
-          log += tmp.ToString() + " ";
-        }
-        Debug.Log(log);
-      }
-    }
-    flag = false;    
-  }
+    public int[,] Map;
 }
